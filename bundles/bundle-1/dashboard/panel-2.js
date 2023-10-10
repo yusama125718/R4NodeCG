715b88f0aa4d8b1ab8124d6520de5c1ceb7e1131
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2023-10-10 20:34:48 +0900

    teamEdit完成

--- bundles/bundle-1/dashboard/panel-2.js
+++ bundles/bundle-1/dashboard/panel-2.js
@@ -1,8 +1,21 @@
-const teams = nodecg.bundleConfig.teams
-addteams()
+var api_url = nodecg.bundleConfig.api; //生成したAPIのURLを指定
+ 
+const teams = []
+fetch(api_url)
+.then(function (fetch_data) {
+    return fetch_data.json()
+})
+.then(function (json) {
+    for (const i in json) {
+        if (json[i] == null) continue;
+        teams.push(json[i])
+    }
+})
+.then(function(){addteams()})
 
 function addteams(){
     for(const i in teams) {
+        if (i == null) continue
         const red = document.getElementById("teams")
         const roption = document.createElement("option")
         roption.text = teams[i].name;
