dfe7313a7580d1d648b3bde4067f5df7e68d46e3
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2023-10-16 10:38:19 +0900

    完成

--- bundles/bundle-1/graphics/index.js
+++ bundles/bundle-1/graphics/index.js
@@ -9,7 +9,7 @@ nodecg.Replicant('redteam').on('change', (newVal) => {
 
     for (const i in newVal.players){
         const p = document.createElement("h3")
-        p.appendChild(document.createTextNode(newVal.players[i].name))
+        p.appendChild(document.createTextNode(newVal.players[i]))
         red.appendChild(p)
     }
 })
@@ -24,7 +24,52 @@ nodecg.Replicant('blueteam').on('change', (newVal) => {
 
     for (const i in newVal.players){
         const p = document.createElement("h3")
-        p.appendChild(document.createTextNode(newVal.players[i].name))
+        p.appendChild(document.createTextNode(newVal.players[i]))
         blue.appendChild(p)
+    }
+})
+
+nodecg.Replicant('map').on('change', (newVal) => {
+    document.getElementById("map").innerText = "NowMap : " + newVal.name
+})
+
+nodecg.Replicant('score').on('change', (newVal) => {
+    document.getElementById("redscore").innerText = newVal.red
+    document.getElementById("bluescore").innerText = newVal.blue
+})
+
+nodecg.Replicant('blueAbility').on('change', (newVal) => {
+    document.getElementById("blue_cost").innerText = "Cost : " + newVal.cost
+    const blue = document.getElementById("blue_abilitys")
+
+    while(blue.lastChild){
+        blue.removeChild(blue.lastChild);
+    }
+
+    for (const i in newVal.abilitys){
+        const img = document.createElement("img")
+        img.src = newVal.abilitys[i]
+        img.width = 35
+        img.height = 56
+        img.className = "blueimg"
+        blue.appendChild(img)
+    }
+})
+
+nodecg.Replicant('redAbility').on('change', (newVal) => {
+    document.getElementById("red_cost").innerText = "Cost : " + newVal.cost
+    const red = document.getElementById("red_abilitys")
+
+    while(red.lastChild){
+        red.removeChild(red.lastChild);
+    }
+
+    for (const i in newVal.abilitys){
+        const img = document.createElement("img")
+        img.src = newVal.abilitys[i]
+        img.width = 35
+        img.height = 56
+        img.className = "redimg"
+        red.appendChild(img)
     }
 })
